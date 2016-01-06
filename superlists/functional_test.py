caaced2825ace86122afaac0f1b692f0b806ad99
Demo User <demouser@MacBook-Air.local>
2016-01-06 10:45:19 -0500

    Added a first model for Item.

--- superlists/functional_test.py
+++ superlists/functional_test.py
@@ -11,6 +11,17 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+
+    def check_for_row_in_list_table(self,row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text,[row.text for row in rows])
+
+    def create_inputbox_and_send_keys(self,row_text):
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys(row_text)
+        inputbox.send_keys(Keys.ENTER)
+
     def test_can_start_a_list_and_retrieve_it_later(self):
 
                 # Edith has heard about a coll new online to-do app.
@@ -23,32 +34,36 @@ class NewVisitorTest(unittest.TestCase):
                 header_text = self.browser.find_element_by_tag_name('h1').text
                 self.assertIn('To-Do', header_text)
 
+
                 # She is invited to enter a to-do item straight away
+                self.create_inputbox_and_send_keys('1. Buy peacock feathers')
+
                 inputbox = self.browser.find_element_by_id('id_new_item')
                 self.assertEqual(
                         inputbox.get_attribute('placeholder'),
                         'Enter a to-do item'
                 )
 
+
                 # She types "Buy peacock feathers" into a text box
                 # (Edith's hobby is typing fly-fishing lures)
-                inputbox.send_keys('Buy peacock feathers')
+
 
                 # When she hits enter, the page updates, and now the page lists
-                # "1. Buy peacock feathers" as an item in a to-do lilsts
-                inputbox.send_keys(Keys.ENTER)
 
-                table = self.browser.find_element_by_id('id_list_table')
-                rows = table.find_element_by_tag_name('tr')
-                self.assertTrue(
-                    any(row.text == '1. Buy peacock feathers' for row in rows)
-                )
+
+                self.check_for_row_in_list_table('1. Buy peacock feathers')
 
                 # There is still a text box inviting her to add another item.
                 # She enters 'Use peacock feathers to make fly'
                 # (Edith is very methodolical)
 
+                self.create_inputbox_and_send_keys('2. Use peacock feathers to make fly')
+
                 # The homepage updates again, and now shows both items on her lists
+                self.check_for_row_in_list_table('1. Buy peacock feathers')
+                self.check_for_row_in_list_table('2. Use peacock feathers to make fly')
+
 
                 # Edith wonders whether the site will remember her list. Then she sees
                 # That the site has generated a unique URL for her -- there is some
