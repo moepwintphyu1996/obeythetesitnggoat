73b6fcf474bfbec6d34eb3e12ca98d7471764d8a
Demo User <demouser@MacBook-Air.local>
2016-01-12 10:55:14 -0500

    Separated model and view test.

--- superlists/functional_tests/test_list_item_validation.py
+++ superlists/functional_tests/test_list_item_validation.py
@@ -6,10 +6,27 @@ class ItemValidationTest(TodoFunctionalTest):
     def test_cannot_add_empty_list_item(self):
         #Edith goes to the home page, and accidentally tries to submit an empty list item.
         #She hits "Enter" on the empty input box.
+        self.browser.get(self.live_server_url)
+        self.enter_a_new_item('')
+
         #The homepage refreshes, and there is an error message saying that list items cannot be blank.
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
 
         #She tries again with some text for the item, which now works.
+        self.enter_a_new_item('Buy milk')
+        self.check_for_row_in_list_table('1. Buy milk')
+
         #Perversely tries to enter a second blank item.
+        self.enter_a_new_item('')
+
         #She receives a similar warning on the list page.
+        self.check_for_row_in_list_table('1. Buy milk')
+        error = self.browser.find_element_by_css_selector('.has-error')
+        self.assertEqual(error.text, "You can't have an empty list item")
+
         #And she can correct it by filling some text in.
+        self.enter_a_new_item('Make tea')
+        self.check_for_row_in_list_table('1. Buy milk')
+        self.check_for_row_in_list_table('2. Make tea')
         self.fail('Finish the test!')
--- superlists/lists/tests/test_views.py
+++ superlists/lists/tests/test_views.py
@@ -74,36 +74,6 @@ class ListViewTest(TestCase):
         self.assertNotContains(response, 'other item 1')
         self.assertNotContains(response, 'other item 2')
 
-class ItemAndListModelsTest(TestCase):
-
-    def test_saving_and_retrieving_items_in_list(self):
-
-        new_list = List()
-        new_list.save()
-
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        first_item.list = new_list
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.list = new_list
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(new_list, saved_list)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(second_saved_item.text, 'Item the second')
-        self.assertEqual(first_saved_item.list, new_list)
-        self.assertEqual(second_saved_item.list, new_list)
-
 class NewListTest(TestCase):
 
     def test_saving_a_POST_request(self):
