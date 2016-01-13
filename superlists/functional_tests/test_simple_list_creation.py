9ef42ded917be87c89a5a797fa6a6e4dbe6d81d4
Demo User <demouser@MacBook-Air.local>
2016-01-13 12:05:30 -0500

    Refactored find_in_table

--- superlists/functional_tests/test_simple_list_creation.py
+++ superlists/functional_tests/test_simple_list_creation.py
@@ -24,12 +24,12 @@ class NewVisitorTest(TodoFunctionalTest):
         edith_list_url = self.browser.current_url
 
         self.assertRegexpMatches(edith_list_url, '/lists/.+')
-        self.check_for_row_in_list_table('1. Buy peacock feathers')
+        self.check_for_row_in_list_table('Buy peacock feathers')
 
         self.enter_a_new_item('Use peacock feathers to make a fly')
 
-        self.check_for_row_in_list_table('1. Buy peacock feathers')
-        self.check_for_row_in_list_table('2. Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('Buy peacock feathers')
+        self.check_for_row_in_list_table('Use peacock feathers to make a fly')
 
         self.browser.quit()
         self.browser = webdriver.Firefox()
